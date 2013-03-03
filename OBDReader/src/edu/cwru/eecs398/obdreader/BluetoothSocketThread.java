e7a9c07b003722ff23da71c9fa817c4a8de2ac0f
Schuyler Thompson <skooz@skooz.net>
2013-03-03 11:13:46 -0500

    send basic command to obdsim

--- OBDReader/src/edu/cwru/eecs398/obdreader/BluetoothSocketThread.java
+++ OBDReader/src/edu/cwru/eecs398/obdreader/BluetoothSocketThread.java
@@ -1,8 +1,10 @@
 package edu.cwru.eecs398.obdreader;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 
 import android.bluetooth.BluetoothSocket;
 import android.util.Log;
@@ -30,17 +32,41 @@ public class BluetoothSocketThread extends Thread {
 		
 	}
 	
+	public class WriteThread extends Thread {
+		@Override
+		public void run() {
+			String command = "01 05\r";
+			try {
+				while (true) {
+					outputStream.write(command.getBytes("US-ASCII"));
+					Thread.sleep(1000);
+				}
+			} catch (IOException e) {
+				Log.e("OBD", "Error writing to output stream", e);
+			} catch (InterruptedException e) {
+				Log.e("OBD", "Error writing to output stream", e);
+			}
+		}
+	}
+	
 	@Override
 	public void run() {
 		int read = Integer.MAX_VALUE;
+		WriteThread writeThread = new WriteThread();
+		writeThread.start();
 		try {
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
 			while (true) {
-				if (inputStream.available() > 0) {
+				while (read != '\r') {
 					read = inputStream.read();
-					Log.d("OBD", "Value read from socket is: " + read);
-					outputStream.write(read);
+					os.write(read);
+					//Log.d("OBD", "Value read from socket is: " + read);
 					//break;
 				}
+				String stringRead = new String(os.toByteArray(), "US-ASCII");
+				Log.d("OBD", "Value read from socket is: " + stringRead);
+				os.reset();
+				read = Integer.MAX_VALUE;
 			}
 			
 		} catch (IOException e) {
